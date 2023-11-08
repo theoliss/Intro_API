59fc64262bc1045ee257d094434e30ae6beb8119
xRayz3n <sebastien_lam@hotmail.fr>
2023-11-08 15:02:47 +0100

    adding features in api.py

--- chatroom_project/api.py
+++ chatroom_project/api.py
@@ -4,24 +4,37 @@ from fastapi import FastAPI, HTTPException
 from chatroom_project.models import MessageIn, MessageOut, ChatRoomIn, ChatRoomOut
 
 import uuid
-import datetime
+from datetime import datetime
 
 app = FastAPI()
-chatrooms = {int:[str, str, list]}
+
+
+class Chatroom:
+    def __init__(self, name: str):
+        self.uid = uuid.uuid4()
+        self.name = name
+        self.messages = [Message]
+
+class Message:
+    def __init__(self, author: str, message):
+        self.author = author
+        self.message = message
+        self.uid = uuid.uuid4()
+        self.date = datetime.now()
+
+chatrooms = {int : Chatroom}
 
 @app.post("/message/{room_id}", tags=['message'])
 def post_a_message(room_id: str, message: MessageIn):
-    uid = uuid.uuid4()
-    author = message.author
-    message = message.message
-    date = str(datetime.datetime.now())
-    author_profile_picture = ""
-    to_send = MessageOut(uid, author, message, date, author_profile_picture)
+    if room_id not in chatrooms:
+        raise HTTPException(status_code=404, detail="Chatroom not found")
+    msg = Message(message.author, message.message)
+    chatrooms[room_id].messages.append(msg)
     
 
 @app.get("/message/{room_id}", tags=['message'])
 def get_messages(room_id: str) -> ChatRoomOut:
-    return None #TODO code that
+    return ChatRoomOut(room_id, chatrooms[room_id].name, chatrooms[room_id].messages)
 
 @app.delete("/message/{message_id}", tags=['message'])
 def delete_a_message(message_id: str):
@@ -33,11 +46,10 @@ def delete_chatroom(room_id: str):
         raise HTTPException(status_code=404, detail="Chatroom not found")
     
 
-
 @app.post("/chatroom", tags=['chatroom'])
 def create_a_chatroom(chatroom: ChatRoomIn):
-    id = uuid.uuid4()
-    chatrooms[id] = [chatroom.name, []]
-    return f"Your chatroom has been created under the roomID: {id}"
+    room = Chatroom(chatroom.name)
+    chatrooms[room.uid] = room
+    return f"Your chatroom has been created under the roomID: {room.uid}"
 
 
